a52bb88815715b9cfed810d177ad378435d55e45
RomanMitaki <r.mitaki@yandex.ru>
2024-02-27 21:59:49 +0300

    add styles for Dropdownmenu and fix filter

--- src/components/Dropdownmenu/Dropdownmenu.tsx
+++ src/components/Dropdownmenu/Dropdownmenu.tsx
@@ -1,5 +1,6 @@
-import React, { ChangeEvent, FC, FormEvent, useState } from "react";
+import React, { ChangeEvent, FC, FormEvent } from "react";
 import { TFilter } from "../../utils/types";
+import classes from "./Dropdownmenu.module.css";
 
 type TDropdownMenuProps = {
   handleSubmit: (e: FormEvent<HTMLFormElement>) => void;
@@ -15,9 +16,9 @@ const DropdownMenu: FC<TDropdownMenuProps> = (props) => {
   };
 
   return (
-    <>
+    <div className={classes.DropdownMenu}>
       <div>
-        <h2>Filter:</h2>
+        <h2 style={{ textAlign: "left" }}>Filter:</h2>
         <select
           value={props.filter}
           // @ts-ignore
@@ -30,20 +31,27 @@ const DropdownMenu: FC<TDropdownMenuProps> = (props) => {
         </select>
       </div>
       <div>
-        {props.filter && (
-          <form method="post" onSubmit={props.handleSubmit}>
-            <label htmlFor="inputField">Enter {props.filter}:</label>
-            <input
-              type="text"
-              id="inputField"
-              name={"filterInput"}
-              value={props.filterValue || ""}
-              onChange={handleChange}
-            />
-          </form>
-        )}
+        <form method="post" onSubmit={props.handleSubmit}>
+          <label htmlFor="inputField">
+            {props.filter === "disabled"
+              ? `Click button to disable filters`
+              : `Enter ${props.filter}`}
+            :
+          </label>
+          <input
+            type={props.filter === "price" ? "number" : "text"}
+            id="inputField"
+            name={"filterInput"}
+            value={props.filterValue || ""}
+            onChange={handleChange}
+            disabled={props.filter === "disabled"}
+          />
+          <button className={classes.DropdownMenu__button} type="submit">
+            ok
+          </button>
+        </form>
       </div>
-    </>
+    </div>
   );
 };
 
